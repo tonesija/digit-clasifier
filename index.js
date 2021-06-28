54dafef89e4eacb6d6cb41f090bd1afeae74fac3
Tonio Ercegović <te51681@fer.hr>
2021-06-28 17:34:08 +0200

    gitignore and static index.html start

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello world!')
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'static')))
+
+console.log('Server listening on port 7070.')
+app.listen(7070)
